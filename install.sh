54885ed366e765553d271d9bafb3a1450cadd4ce
Junpei Ikuta <171251543+Iktahana@users.noreply.github.com>
2024-11-29 23:15:12 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update -y
+sudo apt install docker-ce -y
+sudo docker --version
